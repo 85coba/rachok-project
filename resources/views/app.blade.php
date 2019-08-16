de4d006548ff9fafe606c3a94e9a9c9105e30039
85coba <85coba@gmail.com>
2019-08-16 18:04:46 +0300

    create role permission model

--- resources/views/app.blade.php
+++ resources/views/app.blade.php
@@ -11,7 +11,7 @@
 <title>RachokAdmin</title>
 
 <!-- Custom fonts for this template-->
-<link href="{{ asset('css/all.min.css') }}" rel="stylesheet" type="text/css">
+<link href="{{ asset('vendor/fontawesome-free/css/all.min.css') }}" rel="stylesheet" type="text/css">
 <link href="https://fonts.googleapis.com/css?family=Nunito:200,200i,300,300i,400,400i,600,600i,700,700i,800,800i,900,900i" rel="stylesheet">
 
 <!-- Custom styles for this template-->
@@ -22,7 +22,7 @@
     <div class="container">
 
         @yield('content')
-
+        
     </div>
     <!-- Bootstrap core JavaScript-->
   <script src="{{ asset('vendor/jquery/jquery.min.js') }}"></script>
