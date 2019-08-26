9ce261a1a2c6f76a232075ec89e30fba94b1da42
85coba <85coba@gmail.com>
2019-08-26 17:38:38 +0300

    fix deploy erorrs

--- resources/views/admin.blade.php
+++ resources/views/admin.blade.php
@@ -11,7 +11,7 @@
 <title>RachokAdmin</title>
 
 <!-- Custom fonts for this template-->
-<link href="{{ app()->envireonment('local')
+<link href="{{ app()->environment('local')
     ? asset('vendor/fontawesome-free/css/all.min.css')
     : secure_asset('vendor/fontawesome-free/css/all.min.css') }}" rel="stylesheet" type="text/css">
 <link href="https://fonts.googleapis.com/css?family=Nunito:200,200i,300,300i,400,400i,600,600i,700,700i,800,800i,900,900i" rel="stylesheet">
@@ -28,20 +28,20 @@
 
     </div>
     <!-- Bootstrap core JavaScript-->
-    <script src="{{ app()->envireonment('local')
+    <script src="{{ app()->environment('local')
       ? asset('vendor/jquery/jquery.min.js')
       : secure_asset('vendor/jquery/jquery.min.js') }}"></script>
-    <script src="{{ app()->envireonment('local')
+    <script src="{{ app()->environment('local')
       ? asset('vendor/bootstrap/js/bootstrap.bundle.min.js')
       : secure_asset('vendor/bootstrap/js/bootstrap.bundle.min.js') }}"></script>
 
     <!-- Core plugin JavaScript-->
-    <script src="{{ app()->envireonment('local')
+    <script src="{{ app()->environment('local')
       ? asset('vendor/jquery-easing/jquery.easing.min.js')
       : secure_asset('vendor/jquery-easing/jquery.easing.min.js') }}"></script>
 
     <!-- Custom scripts for all pages-->
-    <script src="{{ app()->envireonment('local')
+    <script src="{{ app()->environment('local')
       ? asset('js/sb-admin-2.min.js')
       : secure_asset('js/sb-admin-2.min.js') }}"></script>
 
