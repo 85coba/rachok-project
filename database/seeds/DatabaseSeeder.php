1d5a363dd605936d36b762b0f6bf41f47094a6ec
85coba <85coba@gmail.com>
2019-08-26 13:37:19 +0300

    add seed for admin and roles

--- database/seeds/DatabaseSeeder.php
+++ database/seeds/DatabaseSeeder.php
@@ -11,6 +11,34 @@ class DatabaseSeeder extends Seeder
      */
     public function run()
     {
-        // $this->call(UsersTableSeeder::class);
+        DB::table('users')->insert([
+            'first_name'    =>  'Denys',
+            'last_name'     =>  'Panchuk',
+            'email'         =>  '85coba@gmail.com',
+            'password'      =>  '$2y$10$Wtmr5QPOq/KHUCkDzNJADOePY6dEWYKX2pT6SPK2ybiMRx0E88HQK',
+            'nickname'      =>  'uapanchude'
+        ]);
+
+        DB::table('roles')->insert(
+            ['name' =>  'admin'],
+            ['name' =>  'vendor'],
+            ['name' =>  'bloked']    
+        );
+
+        DB::table('permissions')->insert(
+            ['name' =>  'browse_admin_panel'],
+            ['name' =>  'browse_cabinet'],
+            ['name' =>  'receive_new_orders']
+        );
+
+        DB::table('permission_role')->insert(
+            ['role_id'  => 1, 'permission_id' => 1],
+            ['role_id'  => 1, 'permission_id' => 2],
+            ['role_id'  => 1, 'permission_id' => 3]
+        );
+
+        DB::table('user_role')->insert(
+            ['user_id' => 1, 'role_id' => 1]
+        );
     }
 }
