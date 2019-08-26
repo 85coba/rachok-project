32020aeaa9348fc9bfc78e784acc752ad80c0b23
85coba <85coba@gmail.com>
2019-08-26 18:24:19 +0300

    fix deploy errors

--- resources/views/admin.blade.php
+++ resources/views/admin.blade.php
@@ -11,13 +11,12 @@
 <title>RachokAdmin</title>
 
 <!-- Custom fonts for this template-->
-<link href="{{ app()->environment('local')
-    ? asset('vendor/fontawesome-free/css/all.min.css')
-    : secure_asset('vendor/fontawesome-free/css/all.min.css') }}" rel="stylesheet" type="text/css">
+<link href="{{ url('vendor/fontawesome-free/css/all.min.css')}}" rel="stylesheet" type="text/css">
+
 <link href="https://fonts.googleapis.com/css?family=Nunito:200,200i,300,300i,400,400i,600,600i,700,700i,800,800i,900,900i" rel="stylesheet">
 
 <!-- Custom styles for this template-->
-<link href="{{ asset('css/sb-admin-2.min.css') }}" rel="stylesheet">
+<link href="{{ url('css/sb-admin-2.min.css') }}" rel="stylesheet">
 
 </head>
   <body id="page-top">
@@ -31,6 +30,7 @@
     <script src="{{ app()->environment('local')
       ? asset('vendor/jquery/jquery.min.js')
       : secure_asset('vendor/jquery/jquery.min.js') }}"></script>
+
     <script src="{{ app()->environment('local')
       ? asset('vendor/bootstrap/js/bootstrap.bundle.min.js')
       : secure_asset('vendor/bootstrap/js/bootstrap.bundle.min.js') }}"></script>
